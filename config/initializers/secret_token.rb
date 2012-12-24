d2ead972cd673660d7cecfc129bfe8d1c2d01073
kathrynalpert <kathrynialpert@gmail.com>
2012-12-23 21:00:36 -0700

    Initial commit of static site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jamesalpert::Application.config.secret_token = '0857d25db373d2e4a2974a8c0f4314e59490d8677247e28553108da47178d50f717a7b4ba0e34b088112b60f5afeaadcbc4929a8a25fc8a4330bbd97b97ba068'
